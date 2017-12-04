8f76b920ddadf554dfb911a1814902088a85aca1
Joris <joris.lambrechts@kuleuven.be>
2017-12-04 13:29:34 +0100

    expand multilanguage

--- plugins/MultiLanguage/MultiLanguagePlugin.php
+++ plugins/MultiLanguage/MultiLanguagePlugin.php
@@ -1,61 +1,306 @@
 <?php
-/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
-
-/**
- * GoogleAnalytics plugin class
- *
- * @package GoogleAnalytics
- */
-class MultiLanguagePlugin extends Omeka_Plugin_AbstractPlugin
+class MultilanguagePlugin extends Omeka_Plugin_AbstractPlugin
 {
     protected $_hooks = array(
-        'install',
-        'uninstall'
-    );
+            'install',
+            'uninstall',
+            'config',
+            'config_form',
+            'admin_head',
+            'admin_footer',
+            'initialize',
+            'exhibits_browse_sql',
+            'simple_pages_pages_browse_sql'
+            );
 
     protected $_filters = array(
-        'locale',
-    );
+            'locale',
+            'guest_user_links',
+            'admin_navigation_main',
+            );
+
+    protected $_translationTable = null;
 
     protected $locale_code;
 
-    public function hookInstall()
+    public function hookInitialize($args)
     {
-        //set_option('google_analytics_key', '');
+        add_translation_source(dirname(__FILE__) . '/languages');
     }
 
-    public function hookUninstall()
+    public function hookExhibitsBrowseSql($args)
     {
-        //delete_option('google_analytics_key');
+        $this->modelBrowseSql($args, 'Exhibit');
+    }
+
+    public function hookSimplePagesPagesBrowseSql($args)
+    {
+        $this->modelBrowseSql($args, 'SimplePagesPage');
+    }
+
+    public function modelBrowseSql($args, $model)
+    {
+        if (! is_admin_theme()) {
+            $select = $args['select'];
+            $db = get_db();
+            $alias = $db->getTable('MultilanguageContentLanguage')->getTableAlias();
+            $modelAlias = $db->getTable($model)->getTableAlias();
+            $select->joinLeft(array($alias => $db->MultilanguageContentLanguage),
+                            "$alias.record_id = $modelAlias.id", array());
+            $select->where("$alias.record_type = ?", $model);
+            $select->where("$alias.lang = ?", $this->locale_code);
+        }
+    }
+    public function filterGuestUserLinks($links)
+    {
+        $links['Multilanguage'] = array('label' => __('Preferred Language'), 'uri' => url('multilanguage/user-language/user-language'));
+        return $links;
+    }
+
+    public function filterAdminNavigationMain($nav)
+    {
+        $nav['Multilanguage'] = array('label' => __('Preferred Language'), 'uri' => url('multilanguage/user-language/user-language'));
+        $nav['Multilanguage_content'] = array(
+                'label' => __('Multilanguage Content'),
+                'uri'   => url('multilanguage/translations/content-language')
+                );
+        return $nav;
     }
 
     public function filterLocale($locale)
     {
-      //check default (from config)
-      $defaultCodes = "nl_BE";
-      $this->locale_code = "nl_BE";
+        $defaultCodes = Zend_Locale::getDefault();
+        $defaultCode = current(array_keys($defaultCodes));
+        if (empty($locale)) {
+            $this->locale_code = $defaultCode;
+        } else {
+            $this->locale_code = $locale;
+        }
 
-      //check session
-      $langNamespace = new Zend_Session_Namespace('lang');
-      if (isset($langNamespace->lang)):
-        $this->locale_code = $langNamespace->lang;
-      endif;
+        //check default (from config)
+        $defaultCodes = "nl_BE";
+        $this->locale_code = "nl_BE";
 
-      //check url
-      if(isset($_GET['lang'])):
-        $lang = $_GET['lang'];
-      else:
-          return $this->locale_code;
-      endif;
+        //check session
+        $langNamespace = new Zend_Session_Namespace('lang');
+        if (isset($langNamespace->lang)):
+          $this->locale_code = $langNamespace->lang;
+        endif;
 
-      if ($lang == "nl"):
-          $this->locale_code = "nl_BE";
-          $langNamespace->lang = "nl_BE";
-      elseif($lang == "fr"):
-          $this->locale_code = "fr";
-          $langNamespace->lang = "fr";
-      endif;
+        //check url
+        if(isset($_GET['lang'])):
+          $lang = $_GET['lang'];
+        else:
+            return $this->locale_code;
+        endif;
 
-      return $this->locale_code;
+        if ($lang == "nl"):
+            $this->locale_code = "nl_BE";
+            $langNamespace->lang = "nl_BE";
+        elseif($lang == "fr"):
+            $this->locale_code = "fr";
+            $langNamespace->lang = "fr";
+        endif;
+
+
+        /*$this->_translationTable = $this->_db->getTable('MultilanguageTranslation');
+        $user = current_user();
+        $userPrefLanguageCode = false;
+        $userPrefLanguage = false;
+        if ($user) {
+            $prefLanguages = $this->_db->getTable('MultilanguageUserLanguage')->findBy(array('user_id' => $user->id));
+            if ( ! empty($prefLanguages)) {
+                $userPrefLanguage = $prefLanguages[0];
+                $userPrefLanguageCode = $userPrefLanguage->lang;
+                $this->locale_code = $userPrefLanguageCode;
+            }
+        }
+        if (! $userPrefLanguageCode) {
+            $codes = unserialize( get_option('multilanguage_language_codes') );
+            //dump the site's default code to the end as a fallback
+            $codes[] = $defaultCode;
+            $browserCodes = array_keys(Zend_Locale::getBrowser());
+            $match = false;
+            foreach ($browserCodes as $browserCode) {
+                if (in_array($browserCode, $codes)) {
+                    $this->locale_code = $browserCode;
+                    $match = true;
+                    break;
+                }
+            }
+            if (! $match) {
+                // Failed to find browserCode in our language codes.
+                // Try to match a two character code and set it to
+                // the closest equivalent if available.
+                $shortcodes = array();
+                foreach ($codes as $c) {
+                    $shortcodes[] = substr($c, 0, 2);
+                }
+                foreach ($browserCodes as $bcode) {
+                    if (in_array($bcode, $shortcodes)) {
+                        $lenCodes = count($codes);
+                        for ($i = 0; $i < $lenCodes; $i++) {
+                            if (strcmp($bcode, $shortcodes[$i]) == 0) {
+                                $this->locale_code = $codes[$i];
+                                break 2;
+                            }
+                        }
+                    }
+                }
+            }
+        }*/
+        //weird to be adding filters here, but translations weren't happening consistently when it was in setUp
+        //@TODO: check if this oddity is due to setting the priority high
+        $translatableElements = unserialize(get_option('multilanguage_elements'));
+
+        if(is_array($translatableElements)) {
+            foreach($translatableElements as $elementSet=>$elements) {
+                foreach($elements as $element) {
+                    add_filter(array('Display', 'Item', $elementSet, $element), array($this, 'translate'), 1000);
+                    add_filter(array('ElementInput', 'Item', $elementSet, $element), array($this, 'translateField'), 1);
+                    add_filter(array('Display', 'Collection', $elementSet, $element), array($this, 'translate'), 1);
+                    add_filter(array('ElementInput', 'Collection', $elementSet, $element), array($this, 'translateField'), 1);
+                    add_filter(array('Display', 'File', $elementSet, $element), array($this, 'translate'), 1);
+                    add_filter(array('ElementInput', 'File', $elementSet, $element), array($this, 'translateField'), 1);
+                }
+            }
+        }
+        return $this->locale_code;
+    }
+
+    public function hookAdminFooter()
+    {
+        echo "<div id='multilanguage-modal'>
+        <textarea id='multilanguage-translation'></textarea>
+        </div>";
+
+        echo "<script type='text/javascript'>
+        var baseUrl = '" . WEB_ROOT . "';
+        </script>
+        ";
+    }
+
+    public function hookAdminHead()
+    {
+        queue_css_file('multilanguage');
+        queue_js_file('multilanguage');
+    }
+
+    public function hookInstall()
+    {
+        $db = $this->_db;
+        $sql = "
+CREATE TABLE IF NOT EXISTS $db->MultilanguageTranslation (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `element_id` int(11) NOT NULL,
+  `record_id` int(11) NOT NULL,
+  `record_type` tinytext COLLATE utf8_unicode_ci NOT NULL,
+  `locale_code` tinytext COLLATE utf8_unicode_ci NOT NULL,
+  `text` text COLLATE utf8_unicode_ci,
+  `translation` text COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `element_id` (`element_id`,`record_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+                    ";
+
+        $db->query($sql);
+
+        $sql = "
+CREATE TABLE IF NOT EXISTS $db->MultilanguageContentLanguage (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `record_type` tinytext COLLATE utf8_unicode_ci NOT NULL,
+  `record_id` int(10) unsigned NOT NULL,
+  `lang` tinytext COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+        ";
+
+        $db->query($sql);
+
+        $sql = "
+
+CREATE TABLE IF NOT EXISTS $db->MultilanguageUserLanguage (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned NOT NULL,
+  `lang` tinytext COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_id` (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+        ";
+
+        $db->query($sql);
+    }
+
+    public function hookUninstall()
+    {
+        $db = $this->_db;
+        $sql = "DROP TABLE $db->MultilanguageTranslation ";
+        $db->query($sql);
+
+        $sql = "DROP TABLE $db->MultilanguageContentLanguage ";
+        $db->query($sql);
+
+
+        $sql = "DROP TABLE $db->MultilanguageUserLanguage";
+        $db->query($sql);
+
+    }
+
+    public function hookConfig($args)
+    {
+        $post = $args['post'];
+        $elements = array();
+        $elTable = get_db()->getTable('Element');
+        foreach($post['element_sets'] as $elId) {
+            $element = $elTable->find($elId);
+            $elSet = $element->getElementSet();
+            if(!array_key_exists($elSet->name, $elements)) {
+                $elements[$elSet->name] = array();
+            }
+            $elements[$elSet->name][] = $element->name;
+        }
+        set_option('multilanguage_elements', serialize($elements));
+        set_option('multilanguage_language_codes', serialize($post['multilanguage_language_codes']));
+    }
+
+    public function hookConfigForm()
+    {
+        include('config_form.php');
+    }
+
+    public function translateField($components, $args)
+    {
+        $record = $args['record'];
+        $element = $args['element'];
+        $type = get_class($record);
+        $languages = unserialize(get_option('multilanguage_language_codes'));
+        $html = __('Translate to: ');
+        foreach ($languages as $code) {
+            $html .= " <li data-element-id='{$element->id}' data-code='$code' data-record-id='{$record->id}' data-record-type='{$type}' class='multilanguage-code'>$code</li>";
+        }
+        $components['form_controls'] .= "<ul class='multilanguage' >$html</ul>";
+        return $components;
+    }
+
+    public function translate($translateText, $args)
+    {
+        $db = $this->_db;
+        $record = $args['record'];
+        //since I'm being cheap and not differentiating Items vs Collections
+        //or any other ActsAsElementText up above in the filter definitions (themselves weird),
+        //I risk getting null values here
+        //after the filter happens for 'element_text'
+        if (! empty($args['element_text'])) {
+            $elementText = $args['element_text'];
+
+            $elementId = $elementText->element_id;
+
+            $translation = $db->getTable('MultilanguageTranslation')->getTranslation($record->id, get_class($record), $elementId, $this->locale_code, $translateText);
+            if ($translation) {
+                $translateText = $translation->translation;
+            }
+        }
+        return $translateText;
     }
 }
