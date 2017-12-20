2639828541ebaf4ee8d6ccb9acd6fac89a6f82dd
Joris <joris.lambrechts@kuleuven.be>
2017-12-20 16:30:44 +0100

    layout captions

--- plugins/ExhibitBuilder/views/shared/exhibit_layouts/file-text/layout.php
+++ plugins/ExhibitBuilder/views/shared/exhibit_layouts/file-text/layout.php
@@ -10,52 +10,97 @@ $captionPosition = isset($options['captions-position'])
     : 'center';
 ?>
 <div class='row content'>
-  <div class="col-12 col-md-6">
-    <hr align="left">
-    <h1><span class="exhibit-page"><?php echo metadata('exhibit_page', 'title'); ?></h1>
-    <?php echo $text; ?>
-  </div>
-  <div class="col-12 col-md-6">
-    <div class="exhibit-items <?php echo $position; ?> <?php echo $size; ?> captions-<?php echo $captionPosition; ?>">
-        <?php foreach ($attachments as $attachment):
-            $item = $attachment->getItem();
-            $file = $attachment->getFile();
-            $fileOptions = array('imageSize' => $size);
-            $linkProps = array(); $forceImage = false;
-
-            $caption = $this->exhibitAttachmentCaption($attachment);
-
-            if(!$caption):
-              $caption = '<div class="exhibit-item-caption">'.metadata($item, array('Dublin Core', 'Title')).'</div>';
-            endif;
-
-            if ($file) {
-                if (!isset($fileOptions['imgAttributes']['alt'])) {
-                    $fileOptions['imgAttributes']['alt'] = metadata($item, array('Dublin Core', 'Title'), array('no_escape' => true));
-                }
-
-                if (!isset($fileOptions['linkAttributes']['href'])) {
-                    $fileOptions['linkAttributes']['href'] = file_display_url($file);
-                }
-                $fileOptions['linkAttributes']['data-lightbox'] = file_display_url($file);
-                $fileOptions['linkAttributes']['data-title'] = $caption;
-                $html = file_markup($file, $fileOptions, null);
-
-            } else if($item) {
-                $html = exhibit_builder_link_to_exhibit_item(null, array('class'=>'test','data-lightbox'=>'test',' data-title'=>'test'), $item);
-            }
-            // Don't show a caption if we couldn't show the Item or File at all
-
-
-            if (isset($html)) {
-                $html .= $caption;
-                echo $html;
-            } else {
-                echo $html = '';
-            }
-
-            //echo $this->exhibitAttachment($attachment, array('imageSize' => $size)); ?>
-        <?php endforeach; ?>
+  <?php if($position == "right"):?>
+    <div class="col-12 col-md-6">
+      <hr align="left">
+      <h1><span class="exhibit-page"><?php echo metadata('exhibit_page', 'title'); ?></h1>
+      <?php echo $text; ?>
     </div>
-  </div>
+    <div class="col-12 col-md-6">
+      <div class="exhibit-items <?php echo $position; ?> <?php echo $size; ?> captions-<?php echo $captionPosition; ?>">
+          <?php foreach ($attachments as $attachment):
+              $item = $attachment->getItem();
+              $file = $attachment->getFile();
+              $fileOptions = array('imageSize' => $size);
+              $linkProps = array(); $forceImage = false;
+
+              $caption = $this->exhibitAttachmentCaption($attachment);
+
+              if(!$caption):
+                $caption = '<div class="exhibit-item-caption">'.metadata($item, array('Dublin Core', 'Title')).'</div>';
+              endif;
+
+              if ($file) {
+                  if (!isset($fileOptions['imgAttributes']['alt'])) {
+                      $fileOptions['imgAttributes']['alt'] = metadata($item, array('Dublin Core', 'Title'), array('no_escape' => true));
+                  }
+
+                  if (!isset($fileOptions['linkAttributes']['href'])) {
+                      $fileOptions['linkAttributes']['href'] = file_display_url($file);
+                  }
+                  $fileOptions['linkAttributes']['data-lightbox'] = file_display_url($file);
+                  $fileOptions['linkAttributes']['data-title'] = $caption;
+                  $html = file_markup($file, $fileOptions, null);
+
+              } else if($item) {
+                  $html = exhibit_builder_link_to_exhibit_item(null, array('class'=>'test','data-lightbox'=>'test',' data-title'=>'test'), $item);
+              }
+              // Don't show a caption if we couldn't show the Item or File at all
+              if (isset($html)) {
+                  $html .= $caption;
+                  echo $html;
+              } else {
+                  echo $html = '';
+              }
+              //echo $this->exhibitAttachment($attachment, array('imageSize' => $size)); ?>
+          <?php endforeach; ?>
+      </div>
+    </div>
+  <?php else: ?>
+    <div class="col-12 col-md-6">
+      <div class="exhibit-items <?php echo $position; ?> <?php echo $size; ?> captions-<?php echo $captionPosition; ?>">
+          <?php foreach ($attachments as $attachment):
+              $item = $attachment->getItem();
+              $file = $attachment->getFile();
+              $fileOptions = array('imageSize' => $size);
+              $linkProps = array(); $forceImage = false;
+
+              $caption = $this->exhibitAttachmentCaption($attachment);
+
+              if(!$caption):
+                $caption = '<div class="exhibit-item-caption">'.metadata($item, array('Dublin Core', 'Title')).'</div>';
+              endif;
+
+              if ($file) {
+                  if (!isset($fileOptions['imgAttributes']['alt'])) {
+                      $fileOptions['imgAttributes']['alt'] = metadata($item, array('Dublin Core', 'Title'), array('no_escape' => true));
+                  }
+
+                  if (!isset($fileOptions['linkAttributes']['href'])) {
+                      $fileOptions['linkAttributes']['href'] = file_display_url($file);
+                  }
+                  $fileOptions['linkAttributes']['data-lightbox'] = file_display_url($file);
+                  $fileOptions['linkAttributes']['data-title'] = $caption;
+                  $html = file_markup($file, $fileOptions, null);
+
+              } else if($item) {
+                  $html = exhibit_builder_link_to_exhibit_item(null, array('class'=>'test','data-lightbox'=>'test',' data-title'=>'test'), $item);
+              }
+              // Don't show a caption if we couldn't show the Item or File at all
+              if (isset($html)) {
+                  $html .= $caption;
+                  echo $html;
+              } else {
+                  echo $html = '';
+              }
+              //echo $this->exhibitAttachment($attachment, array('imageSize' => $size)); ?>
+          <?php endforeach; ?>
+      </div>
+    </div>
+    <div class="col-12 col-md-6">
+      <hr align="left">
+      <h1><span class="exhibit-page"><?php echo metadata('exhibit_page', 'title'); ?></h1>
+      <?php echo $text; ?>
+    </div>
+  <?php endif;?>
 </div>
