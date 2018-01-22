f2ac16341039494c8e81c22dca609eda6248653f
Joris <joris.lambrechts@kuleuven.be>
2018-01-22 14:49:29 +0100

    add tags

--- themes/default/items/browse.php
+++ themes/default/items/browse.php
@@ -29,32 +29,33 @@ echo head(array('title'=>$pageTitle,'bodyclass' => 'items browse'));
          </p>
      </div>
    </div>
-   <div class='row top'>
-     <div class="col-sm-12 col-xs-12">
-         <h1><?php echo $pageTitle;?></h1>
-         <?php if ($total_results > 0): ?>
-             <?php
-             $sortLinks[__('Title')] = 'Dublin Core,Title';
-             $sortLinks[__('Creator')] = 'Dublin Core,Creator';
-             $sortLinks[__('Date Added')] = 'added';
-             ?>
-             <div id="sort-links">
-                 <span class="sort-label"><i class="material-icons">&#xE164;</i> <?php echo __('Sort by: '); ?></span><?php echo browse_sort_links($sortLinks); ?>
-                 <span class="sort-label"><i class="material-icons">&#xE152;</i>
-                    <?php echo __('Filter: '); ?></span>
-                    <?php if($show_featured):?>
-                      <a href="<?php echo url('items/browse');?>">
-                        <?php echo __("All"); ?>
-                      </a>
-                    <?php else:?>
-                      <a href="<?php echo url('items/browse?featured=1');?>">
-                        <?php echo __("Featured"); ?>
-                      </a>
-                    <?php endif;?>
-             </div>
-         <?php endif; ?>
-     </div>
-   </div>
+
+   <h1><?php echo $pageTitle;?></h1>
+
+   <?php if ($total_results > 0): ?>
+       <?php
+       $sortLinks[__('Title')] = 'Dublin Core,Title';
+       $sortLinks[__('Creator')] = 'Dublin Core,Creator';
+       $sortLinks[__('Date Added')] = 'added';
+       ?>
+       <div id="sort-links">
+           <span class="sort-label"><i class="material-icons">&#xE164;</i> <?php echo __('Sort by: '); ?></span><?php echo browse_sort_links($sortLinks); ?>
+           <span class="sort-label"><i class="material-icons">&#xE152;</i>
+           <?php echo __('Filter:');?></span>
+           <ul id="sort-links-list">
+             <li><a href="<?php echo url('items/browse');?>"><?php echo __("All");?></a></li>
+              <?php if(!$show_featured):?>
+                <li>
+                  <a href="<?php echo url('items/browse?featured=1');?>">
+                    <?php echo __("Featured"); ?>
+                  </a>
+                </li>
+              <?php endif;?>
+            </ul>
+       </div>
+    <?php endif; ?>
+
+    <?php echo item_search_filters(); ?>
 
     <?php //echo item_search_filters(); ?>
     <?php echo pagination_links(); ?>
@@ -77,14 +78,14 @@ echo head(array('title'=>$pageTitle,'bodyclass' => 'items browse'));
                 <hr>
                 <div class="card-block">
                   <div class="list-item">
-                    <h3 class="star"><i class="material-icons">&#xE83A;</i><span><?php echo __('Featured');?></span></h3>
+                    <!--<h3 class="star"><i class="material-icons">&#xE83A;</i><span><?php echo __('Featured');?></span></h3>-->
                     <h2><?php echo link_to_item(metadata('item', array('Dublin Core', 'Title')), array('class'=>'permalink')); ?></h2>
 
-                    <!--<?php if (metadata('item', 'has tags')): ?>
-                    <div class="tags"><p><strong><?php echo __('Tags'); ?>:</strong>
-                        <?php echo tag_string('items'); ?></p>
+                    <?php if (metadata('item', 'has tags')): ?>
+                    <div class="tags"></strong>
+                        <?php echo tag_string('items'); ?>
                     </div>
-                    <?php endif; ?>-->
+                    <?php endif; ?>
 
                     <?php fire_plugin_hook('public_items_browse_each', array('view' => $this, 'item' =>$item)); ?>
                   </div>
