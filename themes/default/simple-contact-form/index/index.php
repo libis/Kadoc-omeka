5cde008b6f4ae0f97bce2269c397de2de9c759b5
Joris <joris.lambrechts@kuleuven.be>
2018-02-14 15:19:04 +0100

    Vertaling contact

--- themes/default/simple-contact-form/index/index.php
+++ themes/default/simple-contact-form/index/index.php
@@ -11,7 +11,7 @@ echo head(array('bodyclass' => 'contact'));
 						<div class="col-sm-12 col-xs-12">
 							<p id="simple-pages-breadcrumbs">
 								<span>
-									<a href="http://libis.be/">Startpagina</a>
+									<a href="http://libis.be/"><?php echo __("Home");?></a>
 									> Contact
 									</p>
 								</span></p>
@@ -20,23 +20,23 @@ echo head(array('bodyclass' => 'contact'));
 					<div class='row content'>
 						<div class="col-sm-12 col-xs-12">
 										<div id="form-instructions">
-											<?php echo get_option('simple_contact_form_contact_page_instructions'); // HTML ?>
+											<?php echo __(get_option('simple_contact_form_contact_page_instructions')); // HTML ?>
 										</div>
 										<?php echo flash(); ?>
 										<form name="contact_form" id="contact-form"  method="post" enctype="multipart/form-data" accept-charset="utf-8">
 												<fieldset>
 									        <div class="field">
-														<h3><?php echo $this->formLabel('name', 'Naam: ');?></h3>
+														<h3><label><?php echo __("Name");?></label></h3>
 														<?php echo $this->formText('name', $name, array('class'=>'textinput')); ?>
 													</div>
 
 									        <div class="field">
-									            <h3><?php echo $this->formLabel('email', 'E-mail: ');?></h3>
+									            <h3><label><?php echo __("Email");?></label></h3>
 											    		<?php echo $this->formText('email', $email, array('class'=>'textinput'));  ?>
 									        </div>
 
 													<div class="field">
-													  	<h3><?php echo $this->formLabel('message', 'Bericht: ');?></h3>
+													  	<h3><label><?php echo __("Message");?></label></h3>
 													    <?php echo $this->formTextarea('message', $message, array('class'=>'textinput')); ?>
 													</div>
 											</fieldset>
@@ -47,7 +47,7 @@ echo head(array('bodyclass' => 'contact'));
 													</div>
 
 													<div class="field">
-													  <?php echo $this->formSubmit('send', 'Verstuur'); ?>
+													  <?php echo $this->formSubmit('send', __("Send")); ?>
 													</div>
 									    </fieldset>
 										</form>
