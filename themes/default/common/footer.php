3917528d423c208b358d0fee4c0eb0a268e014ec
Joris <joris.lambrechts@kuleuven.be>
2018-02-27 11:10:39 +0100

    google analytics

--- themes/default/common/footer.php
+++ themes/default/common/footer.php
@@ -64,6 +64,14 @@
     });
 
     </script>
-</body>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
 
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-3238091-18"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'UA-3238091-18');
+    </script>
+</body>
 </html>
