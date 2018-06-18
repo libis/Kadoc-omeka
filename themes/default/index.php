4255a668849cadf2395d2919d4a42c88b897ed9d
Joris <joris.lambrechts@kuleuven.be>
2018-06-18 12:33:57 +0200

    video width + link placeholder

--- themes/default/index.php
+++ themes/default/index.php
@@ -1,4 +1,13 @@
-<?php echo head(array('bodyid'=>'home', 'bodyclass' =>'two-col')); ?>
+<?php echo head(array('bodyid'=>'home', 'bodyclass' =>'two-col'));
+$lang = get_language();
+if($lang == "en"):
+  $tag = "heritage+in+the+spotlight";
+  $sort = "Story title";
+else:
+  $tag = "erfgoed+in+de+kijker";
+  $sort = "Verhaal titel";
+endif;
+?>
 
 <section class="home">
     <div id="content" class='container' role="main" tabindex="-1">
@@ -28,15 +37,15 @@
             </div>
         </div>
         <div class="co col-md-6 col-sm-6 col-lg-4">
-          <a class="block-link" href="<?php echo url("items/browse?tags=erfgoed+in+de+kijker&sort_field=Item+Type+Metadata%2CVerhaal%20titel");?>"><h2><?php echo __('Heritage in the spotlight');?></h2></a>
+          <a class="block-link" href="<?php echo url('items/browse?tags='.$tag.'&sort_field=Item+Type+Metadata%2C'.$sort);?>"><h2><?php echo __('Heritage in the spotlight');?></h2></a>
             <div class="col-content">
-              <a class="block-link" href="<?php echo url("items/browse?tags=erfgoed+in+de+kijker&sort_field=Item+Type+Metadata%2CVerhaal%20titel");?>"><img src="<?php echo img('placeholder3.jpg');?>"></a>
+              <a class="block-link" href="<?php echo url('items/browse?tags='.$tag.'&sort_field=Item+Type+Metadata%2C'.$sort);?>"><img src="<?php echo img('placeholder3.jpg');?>"></a>
               <div class="summary">
                 <p><?php echo __("Here we present surprising ‘objects’ of heritage held in KADOC's capacious collections, which are as varied in form as in content . They provide glimpses of neglected or forgotten facets of the rich history of Flanders.");?></p>
               </div>
             </div>
             <div class="col-footer">
-              <a href="<?php echo url("items/browse?tags=erfgoed+in+de+kijker&sort_field=Item+Type+Metadata%2CVerhaal%20titel");?>"><?php echo __('Discover our heritage objects');?></a>
+              <a href="<?php echo url('items/browse?tags='.$tag.'&sort_field=Item+Type+Metadata%2C'.$sort);?>"><?php echo __('Discover our heritage objects');?></a>
             </div>
         </div>
       </div>
